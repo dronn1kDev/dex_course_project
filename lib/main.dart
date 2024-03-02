bbf94c8f3a625653f8c0c299faa938910b99e37a
Andrey Vakarchuk <dronn1k.dev@gmail.com>
2024-03-03 01:16:11 +0200

    Some changes.

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // some changes
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.red,
