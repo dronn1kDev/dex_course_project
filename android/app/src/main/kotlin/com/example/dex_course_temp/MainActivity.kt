ab63f47d89c519487b44120714224472a7bdc22c
Andrey Vakarchuk <dronn1k.dev@gmail.com>
2024-03-03 01:08:48 +0200

    initial commit.

---
+++ android/app/src/main/kotlin/com/example/dex_course_temp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dex_course_temp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/some_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SomeWidget extends StatelessWidget {
+  final String text;
+
+  const SomeWidget({
+    super.key,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.greenAccent[200],
+      child: Text(
+        text,
+        style: const TextStyle(fontSize: 32),
+      ),
+    );
+  }
+}
